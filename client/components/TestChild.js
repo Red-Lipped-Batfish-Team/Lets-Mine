a32e977d66e1bcbb2ce366f7a5adb334e7ef84b7
han900204 <54014070+han900204@users.noreply.github.com>
2021-12-29 17:42:47 -0800

    updates

---
+++ client/components/TestChild.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestChild = () => {
+  return (
+    <>
+      <h4>We are Crypto Gods!!</h4>
+    </>
+  );
+};
+
+export default TestChild;
--- server/server.js
+++ server/server.js
@@ -1,49 +1,21 @@
-const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const path = require("path");
 
-//REQUIRE ROUTER//
+const PORT = process.env.PORT || 3000;
 
+// Statically serve everything in the build folder on the route '/public'
+if (process.env.NODE_ENV === "production") {
+  app.use("/public", express.static(path.join(__dirname, "../public")));
 
-app.use(express.json()); // => req.body
-app.use(express.urlencoded({ extended: true }));
+  // Serve index.html on the route '/'
+  app.use("/", (req, res) => {
+    return res
+      .status(200)
+      .sendFile(path.join(__dirname, "../public/index.html"));
+  });
+}
 
-//ROUTER HANDLER//
-
-
-//STATIC//
-app.use(express.static(path.resolve(__dirname, "../dist")));
-
-app.get("/", (req, res) => {
-  return res.sendFile(path.join(__dirname, "../src/index.html"));
+app.listen(PORT, () => {
+  console.log(`Server is running on the server ${PORT}`);
 });
-
-
-
-
-
-
-// Catch-all route handler for any requests to an unknown route//
-app.use((req, res) => {
-    console.log("404 error triggered");
-    res.sendStatus(404);
-  });
-  
-  //Configure express global error handler//
-  app.use((err, req, res, next) => {
-    const defaultErr = {
-      log: "Express error handler caught unknown middleware error",
-      status: 500,
-      message: { err: "An error occurred" },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-  });
-
-  module.exports = app;
