f06a23db797e353415be250e7d79c1ad93a273eb
Daniel Forrester <danielf216@gmail.com>
2021-12-30 22:31:17 -0500

    Added the started tutorial boiler plate of redux toolkit using counter example Co-authoer-by: Kevin Mun <k.mun94@hotmail.com> Co-authoer-by: Han Kim <han900204@gmail.com> Co-authoer-by: Norman Liu <normanliu91@gmail.com> Co-authoer-by: Daniel Forrester <danielf216@gmail.com>

--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App.jsx";
+import store from './app/store';
+import { Provider } from 'react-redux';
 
-render(<App />, document.getElementById("root"));
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+    );
