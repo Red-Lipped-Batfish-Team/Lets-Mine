5fd4b9463181165f46b0f7d80758fef51bdc7942
Daniel Forrester <danielf216@gmail.com>
2022-01-03 20:18:36 -0500

    Fixed the transactionAPI router for Patch from Delete

--- server/routes/transactionApi.js
+++ server/routes/transactionApi.js
@@ -30,12 +30,12 @@ router.post("/", postTransaction, patchWithTransaction, (req, res) => {
 
 // Patch a Transaction
 router.patch("/:id", patchTransaction, (req, res) => {
-  return res.status(200).json({ transactionId: res.locals.transactionId });
+  return res.status(200).json({ transactionId: res.locals.transactionId, itemId: res.locals.itemId });
 });
 
 // Delete a Transaction
 router.delete("/:id", deleteTransaction, (req, res) => {
-  return res.status(200).json({ transactionId: res.locals.transactionId, itemId: res.locals.itemId });
+  return res.status(200).json({ transactionId: res.locals.transactionId});
 });
 
 module.exports = router;
