fc79061b5df455f5967fba6b606161a9184f228d
normsliu <normanliu91@gmail.com>
2022-01-03 22:49:02 -0500

    updating api for cartpage

--- client/components/Card.js
+++ client/components/Card.js
@@ -11,6 +11,7 @@ const Card = ({ props }) => {
   const { header, body, size } = props;
   const createCart = () => {
     //get user id
+
     //check to see if the current user has a cart id
     //if no cart then we create cart and we patch cart by inserting the item
     //if cart exists we patch cart by inserting the item
--- client/routes/CartPage.js
+++ client/routes/CartPage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CartItem from '../components/CartItem'
 import CartTotal from '../components/CartTotal'
+import axios from 'axios';
 
 const CartPage = () => {
+  const [change, setChange] = useState(false);
+  const token = sessionStorage.getItem(token);
+  console.log(token);
+
+  const getCartItems = async () => {
+    console.log(token);
+    const userId = await axios.post('auth/session', { token }, {headers: {'Content-Type' : 'application/json'}});
+    setChange(true)
+    console.log(userId)
+  }
     //get the user id
+  useEffect(() => {
+    getCartItems();
+  }, [change]);
     //api request to get the cart id 
     //iterate over items 
   
@@ -13,7 +27,7 @@ const CartPage = () => {
     return (
       <div>
         <h1>Cart</h1>
-
+        <h2>{token}</h2>
         {cart.length === 0 ? (
           <div>"cart is empty"</div>
         ) : (
