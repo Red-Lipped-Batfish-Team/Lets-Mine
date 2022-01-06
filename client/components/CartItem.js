6ad4e2acfd4a4c8b36b083af68821151f71eb77e
Kevin Mun <k.mun94@Hotmail.com>
2022-01-05 20:44:39 -0700

    nav bar login/seller

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -4,7 +4,7 @@ import { Button, Card, CardActionArea } from "@material-ui/core";
 //import icons minus, plus, and trash icon
 
 const CartItem = ({ props }) => {
-  const { amount, expired, item_id, quantity, rental_duration } = props;
+  const { amount, expired, item_id, quantity, rental_duration, model } = props;
   //item id
   //increase button would update the cart by searching by item id in cart id
   //update the quantity of that item
@@ -16,6 +16,7 @@ const CartItem = ({ props }) => {
   return (
   <div>
     <ul>
+      <h3>model: {model}</h3>
       <li>item: {item_id}</li>
       <li>quantity: {quantity}</li>
       <li>amount: {amount}</li>
